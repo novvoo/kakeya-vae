3f601ce6d6ea2b3dc304db49b84e23f02e6da149
jingslunt <jingslunt@qq.com>
2026-07-29 14:39:57 +0800

    change

--- src/kakeya/image_codec.py
+++ src/kakeya/image_codec.py
@@ -158,7 +158,7 @@ class KakeyaHyperpriorCodec(nn.Module):
         self.g_s = nn.Sequential(
             weight_norm(nn.Conv2d(latent_dim, 64, 3, padding=1)),  # expand to 64ch
             ResidualBlockGDN(64),
-            weight_norm(nn.Conv2d(64, 32, 3, padding=1)),          # squeeze to 32ch
+            DepthToSpace(64, 32),                                    # upscale + mix
             ResidualBlockGDN(32),
             DepthToSpace(32, 24),
             ResidualBlockGDN(24),
